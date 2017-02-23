4d2839092788151e1fae2180695e17740d91fd55
xjyhehe <328993568@qq.com>
2017-02-23 08:49:51 +0800

    添加定位功能

--- MyApplication/app/src/main/java/com/navigation/guidingapp/MainActivity.java
+++ MyApplication/app/src/main/java/com/navigation/guidingapp/MainActivity.java
@@ -23,17 +23,23 @@ public class MainActivity extends BaseActivity {
         switch (v.getId()){
             case R.id.ib_video:
                 //视频录像
+                Intent videoIntent = new Intent(this,VideoActivity.class);
+                startActivity(videoIntent);
                 break;
             case R.id.ib_navigate:
-                Intent navigateIntent = new Intent(this,NavigationActivity.class);
+                Intent navigateIntent = new Intent(this,MapActivity.class);
                 startActivity(navigateIntent);
                 //导航
                 break;
             case R.id.ib_update:
                 //升级
+                Intent updateIntent = new Intent(this,UpdateActivity.class);
+                startActivity(updateIntent);
                 break;
             case R.id.ib_setting:
                 //设置
+                Intent settingIntent = new Intent(this,SettingActivity.class);
+                startActivity(settingIntent);
                 break;
         }
 
---
+++ MyApplication/app/src/main/java/com/navigation/guidingapp/SettingActivity.java
@@ -0,0 +1,14 @@
+package com.navigation.guidingapp;
+
+import android.os.Bundle;
+
+import com.navigation.guidingapp.base.BaseActivity;
+
+public class SettingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
