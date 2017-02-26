1167d46c4c668b7ff3b1d1ef2773a2cedf66afb4
xjyhehe <328993568@qq.com>
2017-02-26 22:07:06 +0800

    location success

--- MyApplication/app/src/main/java/com/navigation/guidingapp/MainActivity.java
+++ MyApplication/app/src/main/java/com/navigation/guidingapp/MainActivity.java
@@ -1,17 +1,10 @@
 package com.navigation.guidingapp;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.navigation.guidingapp.base.BaseActivity;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Locale;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -23,34 +16,8 @@ public class MainActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        String sha1 = sHA1();
-        Log.i("hehe",sha1);
     }
 
-    public String sHA1() {
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            byte[] cert = info.signatures[0].toByteArray();
-            MessageDigest md = MessageDigest.getInstance("SHA1");
-            byte[] publicKey = md.digest(cert);
-            StringBuffer hexString = new StringBuffer();
-            for (int i = 0; i < publicKey.length; i++) {
-                String appendString = Integer.toHexString(0xFF & publicKey[i])
-                        .toUpperCase(Locale.US);
-                if (appendString.length() == 1)
-                    hexString.append("0");
-                hexString.append(appendString);
-                hexString.append(":");
-            }
-            String result = hexString.toString();
-            return result.substring(0, result.length()-1);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
 
     @OnClick({R.id.ib_video,R.id.ib_navigate,R.id.ib_update,R.id.ib_setting})
     public void buttonClick(View v){
